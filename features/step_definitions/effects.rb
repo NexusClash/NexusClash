572fd6a61fa4782ed7bfba2cd42b70fac6b6fcf2
Rincewind <lyralei@windrunner.mx>
2015-09-08 22:20:44 +1200

    Implement automated tests for weapons as items

--- features/step_definitions/effects.rb
+++ features/step_definitions/effects.rb
@@ -24,6 +24,15 @@ Given(/(\S+) has an innate (\S+) attack \((\d+) (\S+) @ (\d+)%\)/) do |char_name
 	weapon.stateful = character
 end
 
+Given(/(\S+) has an item with a (\S+) attack \((\d+) (\S+) @ (\d+)%\)/) do |char_name, family, amount, type, hit_chance|
+	character = @characters[char_name]
+	item = Entity::Item.new
+	weapon = Entity::Status.new
+	weapon.effects << Effect::Weapon.new(weapon, family.to_sym, hit_chance.to_i, type.to_sym, amount.to_i, 'Test weapon')
+	item.type_statuses = [weapon]
+	item.carrier = character
+end
+
 Given(/(\S+) has an innate ammo-using (\S+) attack \((\d+) (\S+) @ (\d+)%\) loaded with (\d+) ammo/) do |char_name, family, amount, type, hit_chance, ammo|
 	character = @characters[char_name]
 	character.set_tag :ammo, ammo.to_i
@@ -33,9 +42,42 @@ Given(/(\S+) has an innate ammo-using (\S+) attack \((\d+) (\S+) @ (\d+)%\) load
 	weapon.stateful = character
 end
 
-When(/(\S+) attacks (\S+)/) do |char_name, defender_name|
+Given(/(\S+) has an item with an ammo-using (\S+) attack \((\d+) (\S+) @ (\d+)%\) loaded with (\d+) ammo/) do |char_name, family, amount, type, hit_chance, ammo|
+	character = @characters[char_name]
+	character.set_tag :ammo, ammo.to_i
+	item = Entity::Item.new
+	weapon = Entity::Status.new
+	weapon.effects << Effect::WeaponWithAmmo.new(weapon, family.to_sym, hit_chance.to_i, type.to_sym, amount.to_i, 1, 'Out of ammo!', 'Test weapon')
+	weapon.effects << Effect::Reloadable.new(weapon, 'test_ammo', 10)
+	item.type_statuses = [weapon]
+	item.carrier = character
+end
+
+Given(/(\S+) has (\d+) ammo/) do |char_name, amount|
+	character = @characters[char_name]
+	item = Entity::Item.new
+	ammo = Entity::Status.new
+	ammo.effects << Effect::Ammo.new(ammo, 'test_ammo', 10)
+	item.type_statuses = [ammo]
+	item.carrier = character
+end
+
+Given(/(\S+) has a skill that increases hit % of (\S+) weapons by (\d+)%/) do |char_name, family, amount|
+	character = @characters[char_name]
+	buff = Entity::Status.new
+	buff.effects << Effect::WeaponBuff.new(buff, family.to_sym, amount.to_i)
+	buff.stateful = character
+end
+
+When(/(\S+) attacks (\S+) with their weapon/) do |char_name, defender_name|
 	attacker = @characters[char_name]
 	defender = @characters[defender_name]
 	weapon = attacker.weaponry.keys[0]
 	attacker.attack(defender, weapon)
+end
+
+When(/(\S+) reloads their weapon/) do |char_name|
+	char = @characters[char_name]
+	use = char.activated_uses[0]
+	char.use_item_self char.items[0], use.object_id
 end
