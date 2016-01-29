669d48450cb6527e648f2147f638d8a94cf89f69
Rincewind <lyralei@windrunner.mx>
2016-01-30 01:00:16 +1300

    Implemented JVM memory logging for Munin

--- controllers/admin.rb
+++ controllers/admin.rb
@@ -1,3 +1,7 @@
+(require 'java'
+import java.lang.management.ManagementFactory
+)
+
 class Dash < Sinatra::Application
 	get '/admin' do
 		protected! :admin
@@ -36,6 +40,19 @@ class Dash < Sinatra::Application
 
 		haml :'admin/map', :layout => :'layouts/admin', :locals => {}
 
+	end
+
+	get '/admin/memory' do
+
+		mem_bean = ManagementFactory.memory_mx_bean
+		heap = mem_bean.heap_memory_usage.to_s.split
+		heap_used = heap[5].split('(')[0]
+		heap_max = heap[11].split('(')[0]
+		non_heap = mem_bean.non_heap_memory_usage.to_s.split
+		non_heap_used = non_heap[5].split('(')[0]
+		non_heap_max = non_heap[11].split('(')[0]
+
+		haml :'admin/memory', :layout => :'layouts/empty', :locals => {heap_used: heap_used, heap_max: heap_max, non_heap_used: non_heap_used, non_heap_max: non_heap_max}
 	end
 
 end
