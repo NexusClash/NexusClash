51143878a40f2808a272827995deb4b4421e77d4
Rincewind <lyralei@windrunner.mx>
2016-04-24 16:11:42 +1200

    Fix bug preventing skill display from working

--- controllers/character.rb
+++ controllers/character.rb
@@ -53,43 +53,6 @@ class Dash < Sinatra::Application
 				end
 			end
 
-			unassigned = Array.new
-
-			Entity::StatusType.skills.each do |skill|
-				if skill.family == :skill
-					instance_skill = Entity::Status.source_from(skill.id)
-					instance_skill.effects.each do |effect|
-						if effect.is_a?(Effect::SkillPrerequisite) && !skips.include?(skill.id)
-
-							cp_cost = 0
-
-							instance_skill.effects.each do |seffect|
-								cp_cost += seffect.cp_cost if seffect.is_a?(Effect::SkillPurchasable)
-							end
-
-							tree, node_add = add_to_tree.call(root, {id: skill.id, name: instance_skill.name, description: instance_skill.describe, type: instance_skill.family, learned: false, cost: cp_cost, children: []}, effect.link.id)
-							root = tree if node_add
-
-							unless node_add
-								unassigned << [{id: skill.id, name: instance_skill.name, description: instance_skill.describe, type: instance_skill.family, learned: false, cost: cp_cost, children: []}, effect.link.id]
-							end
-
-						end
-					end
-				end
-			end
-
-
-			old_quantity = 0
-			while old_quantity != unassigned.count do
-				unassigned.each do |element|
-
-					tree, node_add = add_to_tree.call(root, element[0], element[1])
-					root = tree if node_add
-
-					unassigned.delete(element) if node_add
-				end
-			end
 
 			root = [root] unless root.is_a? Array
 
