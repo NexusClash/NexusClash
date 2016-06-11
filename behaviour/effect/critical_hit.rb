0085b16b1c50058390ec235649164f05d5cc60a4
Rincewind <lyralei@windrunner.mx>
2016-06-11 18:21:19 +1200

    Resolve ND-16 Critical Hits erroneously apply in the target pane

--- behaviour/effect/critical_hit.rb
+++ behaviour/effect/critical_hit.rb
@@ -8,6 +8,7 @@ module Effect
 			@chance = chance
 			@base_bonus = base_bonus
 			@level_mult = level_mult
+			level_div = 1 if level_div == 0 # Avoid divide by zero errors
 			@level_div = level_div
 			@apply_globally = true
 			unserialise
@@ -20,27 +21,28 @@ module Effect
 		def unserialise
 			if @parent.respond_to? :parent
 				origin = @parent.parent
+				# Critical Hit effects on items only apply to attacks belonging to the weapon
 				@apply_globally = origin === nil || (!origin.is_a?(Entity::Item) && !origin.is_a?(Entity::ItemType))
 			end
 		end
 
-		def alter_attack_intent(intent)
-
+		def intent_combat_hook(intent, step, pov)
 			unless @apply_globally
-				return intent unless intent.weapon.parent == @parent
+				return intent unless intent.attack.weapon.parent == @parent
 			end
-
-			intent.debug self
-			roll = rand(1..100)
-			if roll < chance
-				intent.append_message(' This was a critical hit!')  if intent.hit?
-				damage = base_bonus + (intent.entity.level * level_mult / level_div).floor
-				intent.damage += damage
-				intent.debug "#{chance}% crit roll... #{roll} = CRIT! Increasing damage by #{damage}."
-			else
-				intent.debug "#{chance}% crit roll... #{roll} = Not a crit."
+			if step == :attack_hit && pov == :attacker
+				intent.attack.debug self
+				roll = rand(1..100)
+				if roll < chance
+					intent.attack.append_message(' This was a critical hit!')  if intent.hit?
+					damage = base_bonus + (intent.entity.level * level_mult / level_div).floor
+					intent.attack.damage += damage
+					intent.attack.debug "#{chance}% crit roll... #{roll} = CRIT! Increasing damage by #{damage}."
+				else
+					intent.attack.debug "#{chance}% crit roll... #{roll} = Not a crit."
+				end
+				return intent
 			end
-			return intent
 		end
 
 		def describe
